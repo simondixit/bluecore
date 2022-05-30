fb0a162882fd95e740589c2d93095d32856a6f4e
simondixit <simondx777@gmail.com>
2022-05-30 18:45:20 -0400

    more widgets

--- main.py
+++ main.py
@@ -12,13 +12,37 @@ app.config.update(
 def index():
     return render_template('index.html')
 
+@app.route("/button")
+def button():
+    return render_template('button.html')
+
+@app.route("/order-number-and-date")
+def orderNumberAndDate():
+    return render_template('order-number-and-date.html')
+
+@app.route("/returns-or-damages")
+def returnOrDamages():
+    return render_template('returns-or-damages.html')
+
+@app.route("/items-displayer")
+def itemsDisplayer():
+    return render_template('items-displayer.html')
+
 @app.route("/order-details")
 def orderDetails():
     return render_template('order-details.html')
 
 @app.route("/section-divider")
-def sectionDividerWidget():
+def sectionDivider():
     return render_template('section-divider.html')
+
+@app.route("/green-delivery")
+def greenDelivery():
+    return render_template('green-delivery.html')
+
+@app.route("/footer")
+def footer():
+    return render_template('footer.html')
 
 server = Server(app.wsgi_app)
 # server.watch
