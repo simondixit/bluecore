206d2fae6b489d1f84ecd939db34285126db4f51
simondixit <simondx777@gmail.com>
2022-06-16 14:11:06 -0400

    latest updates (responsive, bluecore-reset, final schema)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from livereload import Server
 
 app = Flask(__name__)
@@ -9,40 +9,15 @@ app.config.update(
 )
 
 @app.route("/")
-def index():
+def default():
     return render_template('index.html')
 
-@app.route("/button")
-def button():
-    return render_template('button.html')
-
-@app.route("/order-number-and-date")
-def orderNumberAndDate():
-    return render_template('order-number-and-date.html')
-
-@app.route("/returns-or-damages")
-def returnOrDamages():
-    return render_template('returns-or-damages.html')
-
-@app.route("/items-displayer")
-def itemsDisplayer():
-    return render_template('items-displayer.html')
-
-@app.route("/order-details")
-def orderDetails():
-    return render_template('order-details.html')
-
-@app.route("/section-divider")
-def sectionDivider():
-    return render_template('section-divider.html')
-
-@app.route("/green-delivery")
-def greenDelivery():
-    return render_template('green-delivery.html')
-
-@app.route("/footer")
-def footer():
-    return render_template('footer.html')
+@app.route("/<string:path>")
+def index(path):
+    if path != "":
+        return render_template(path + '.html')
+    else:
+        abort(404)
 
 server = Server(app.wsgi_app)
 # server.watch
